4cf182af3792fe1c6a9808163bb0215035d01b95
cyyber <kaushal.forex@gmail.com>
2026-04-03 03:22:48 +0400

    remove browserify & update dependencies version

--- scripts/build-browser-tests.sh
+++ scripts/build-browser-tests.sh
@@ -6,9 +6,6 @@ npx tsc --project tsconfig.json
 echo "Building tests with Parcel"
 npx parcel build --no-cache --no-optimize test-builds/tsc/test/test-vectors/*.js --dist-dir test-builds/parcel --target parcel_tests
 
-echo "Building tests with Browserify"
-npx browserify test-builds/tsc/test/test-vectors/*.js > test-builds/browserify-build.js
-
 echo "Building tests with webpack"
 npx webpack --mode development ./test-builds/tsc/test/test-vectors/*.js --output-path ./test-builds
 
